83987cd190ecfed4551d3ed2ef6110fae1b0aeac
Paul Makles <paulmakles@gmail.com>
2022-06-18 22:09:22 +0100

    feat: add GraphQL

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+// path: ./config/plugins.js
+
+module.exports = {
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
